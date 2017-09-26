a22445efbfefda5bf20fa35d29b25486bbbf3a4d
rabota85 <rabota85@abv.bg>
2017-09-26 12:56:12 +0300

    Add project files.

---
+++ helloName/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
